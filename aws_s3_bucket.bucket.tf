bc734de4104f80fc1b6a033fc62effa66c7ad2ce
Sion Smith <sion.smith@gmail.com>
2019-01-16 07:12:34 +0000

    configuring optional SSE encryption

--- aws_s3_bucket.bucket.tf
+++ aws_s3_bucket.bucket.tf
@@ -5,8 +5,8 @@ resource "aws_s3_bucket" "bucket" {
   force_destroy = "${var.s3_bucket_force_destroy}"
 
   server_side_encryption_configuration {
-    count = 1
     rule {
+      count = 1
       apply_server_side_encryption_by_default {
         sse_algorithm = "AES256"
       }
