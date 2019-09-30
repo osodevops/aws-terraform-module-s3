63eef26783f82e4c4872048a410e888bf7c1fe03
Richard Gutkowski <richard@ososolution.com>
2019-09-30 11:45:50 +0100

    added a lifecycle policy

--- aws_s3_bucket.tf
+++ aws_s3_bucket.tf
@@ -6,6 +6,7 @@ resource "aws_s3_bucket" "bucket" {
 
   versioning {
     enabled = "${var.bucket_versioning}"
+    mfa_delete = "${var.mfa_delete_enabled}"
   }
 
   server_side_encryption_configuration {
@@ -19,4 +20,37 @@ resource "aws_s3_bucket" "bucket" {
   tags = "${merge(var.common_tags,
     map("Name", "${local.environment}-${var.s3_bucket_name}-S3")
     )}"
+
+  #lifecycle rules for non-current versions (defaults to on)
+  lifecycle_rule {
+    enabled = "${var.enable_lifecycle}"
+    id      = "default"
+
+    abort_incomplete_multipart_upload_days = 14
+
+    transition {
+      days          = "${var.current_ia_transition_days}"
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = "${var.current_glacier_transition_days}"
+      storage_class = "GLACIER"
+    }
+
+    noncurrent_version_transition {
+      days          = "${var.noncurrent_ia_transition_days}"
+      storage_class = "STANDARD_IA"
+    }
+
+    noncurrent_version_transition {
+      days          = "${var.noncurrent_glacier_transition_days}"
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      expired_object_delete_marker = "${var.delete_expired_objects}"
+      days          = "${var.current_version_expiration_days}"
+    }
+  }
 }
--- variables.tf
+++ variables.tf
@@ -21,3 +21,35 @@ variable "bucket_versioning" {
 variable "s3_sse_algorithm" {
   default = "AES256"
 }
+
+variable "mfa_delete_enabled" {
+  default = false
+}
+
+variable "enable_lifecycle" {
+  default = true
+}
+
+variable "current_ia_transition_days" {
+  default = 30
+}
+
+variable "current_glacier_transition_days" {
+  default = 60
+}
+
+variable "noncurrent_ia_transition_days" {
+  default = 30
+}
+
+variable "noncurrent_glacier_transition_days" {
+  default = 60
+}
+
+variable "delete_expired_objects" {
+  default = false
+}
+
+variable "current_version_expiration_days" {
+  default = 90
+}
